986fca675a69626ab3994ca93aaad549904eb2ef
Hoon Cho <hoon.cho@ascentllc.local>
2021-08-06 08:23:21 -0500

    commited from work computer

--- anagram_detection.py
+++ anagram_detection.py
@@ -1,5 +1,6 @@
 def is_anagram(test, original):
-    test_list = list(test).lower().sort()
-    return test_list
-
+    test_list = sorted(list(test.lower()))
+    original_list = sorted(list(original.lower()))
+    return test_list == original_list
+    
 print(is_anagram("Buckethead", "DeathCubeK"))
