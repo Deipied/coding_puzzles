1d1c0ef72433586af063bcd986a4b483a6d59297
Hoon Cho <hoonhyungcho@gmail.com>
2021-08-09 09:53:57 -0500

    solved find the parity outlier 6kyu

--- practice.py
+++ practice.py
@@ -1,10 +1,19 @@
-def count_bits(n):
-    binary_str = str(format(n, "b"))
-    i = 0
-    for int in binary_str:
-        if int == "1":
-            i += 1
-    return i 
+def find_outlier(integers):
+    even_list = []
+    odd_list = []
+    for int in integers:
+        if int%2 == 0:
+            even_list.append(int)
+        else:
+            odd_list.append(int)
+    if len(even_list) > len(odd_list):
+        return odd_list[0]
+    else:
+        return even_list[0]
+    
+    
+        
+    
 
 
-print(count_bits(10))
+print(find_outlier([2, 4, 6, 8, 10, 3]))
