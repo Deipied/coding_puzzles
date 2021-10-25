3378a93e6a0eec785265037b81e887480928e1bd
Hoon Cho <hoonhyungcho@gmail.com>
2021-10-25 11:09:07 -0500

    initialized coding puaazle add two numbers

--- move_zeroes.py
+++ move_zeroes.py
@@ -11,6 +11,21 @@ def moveZeroes(nums):
         count -= 1
     return nums
 
+# Two pointer solutoin with one looping through and another pos looping through separately switching indices
+# class Solution:
+#     def moveZeroes(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: void Do not return anything, modify nums in-place instead.
+#         """
+#         pos = 0
+        
+#         for i in range(len(nums)):
+#             el = nums[i]
+#             if el != 0:
+#                 nums[pos], nums[i] = nums[i], nums[pos]
+#                 pos += 1
+
 
 
 nums = [0,1,0,3,12]
