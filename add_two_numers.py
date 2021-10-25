7d1a16b52fe890cd7ce1e012edf2b7184db9e5c2
Hoon Cho <hoonhyungcho@gmail.com>
2021-10-25 13:28:29 -0500

    made progress but ultimately did not get the right answer due to listnodes having no len() attribute

--- add_two_numers.py
+++ add_two_numers.py
@@ -3,5 +3,25 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+def addTwoNumbers(l1,l2):
+    while len(l1) > len(l2):
+        l2.append(0)
+    while len(l2) < len(l2):
+        l1.append(0)
+    carry = 0
+    ans = []
+    for i in range(len(l1)):
+        sum = carry + l1[i] + l2[i]
+        if sum > 9:
+            ans.append(sum - 10)
+            carry = 1
+            if i == len(l1)-1:
+                ans.append(1)
+        else:
+            ans.append(sum)
+            carry = 0
+    return ans
+
+l1 = [9,9,9,9]
+l2 = [9,9]
+print(addTwoNumbers(l1,l2))
