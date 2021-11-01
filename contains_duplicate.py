23ca52c9ae106dd04ccb9c53f39c25a0a8992559
Hoon Cho <hoonhyungcho@gmail.com>
2021-11-01 11:18:58 -0500

    solved and finalized find duplicate

--- contains_duplicate.py
+++ contains_duplicate.py
@@ -2,7 +2,7 @@ def findDuplicate(nums):
     if len(nums) == 1:
         return False
     ans = {}
-    for i in range(len(nums):
+    for i in range(len(nums)):
         if nums[i] in ans:
             return True
         else:
